5f6465d17c6820f3ef69a4eb28bd182564f60b26
ubuntuegor <egorxxx29@gmail.com>
2018-03-23 19:05:39 +0300

    v1.0.2: Got rid of request module

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const request = require('request'),
+const http = require('http'),
     fork = require('child_process').fork,
     settings = require('./config.json');
 
@@ -17,33 +17,48 @@ function getPresence(track){
 }
 
 function update(){
-    request(settings.lastfmBase+"?method=user.getrecenttracks&user="+settings.username+"&api_key="+settings.lastfmKey+"&format=json&limit=1", function (error, response, body) {
-        if (error){
-            console.error(error);
+    http.get(settings.lastfmBase+"?method=user.getrecenttracks&user="+settings.username+"&api_key="+settings.lastfmKey+"&format=json&limit=1", (res) => {
+        var { statusCode } = res;
+        if (statusCode !== 200) {
+            console.error (`Request Failed.\nStatus Code: ${statusCode}`);
+            // consume response data to free up memory
+            res.resume();
             return;
         }
-        var track = JSON.parse(body).recenttracks.track[0];
-        if (track["@attr"]&&track["@attr"].nowplaying=="true"&&track.url!=lastSongUrl){
-            if (!childProc||childProc.killed) {
-                childProc = fork('update.js', [], {
-                    detached: true
-                  });
-                childProc.on("message", function(m){
-                	if (m=="started") childProc.send(getPresence(track));
-                	else console.log(m);
-                });
-                childProc.on("exit", function(){
-                	console.log("Player stopped");
-                });
+
+        res.setEncoding('utf8');
+        var response = '';
+        res.on('data', (chunk) => { response += chunk; });
+        res.on('end', () => {
+            try {
+                var track = JSON.parse(response).recenttracks.track[0];
+            } catch (e) {
+                console.error(e.message);
             }
-            else childProc.send(getPresence(track));
-       		console.log("Track updated: "+ track.artist["#text"]+" - "+track.name);
-            lastSongUrl = track.url;
-        }
-        else if (childProc&&!childProc.killed&&lastSongUrl!="0"&&!track["@attr"]) {
-            childProc.kill();
-            lastSongUrl = "0";
-        }
+            if (track["@attr"]&&track["@attr"].nowplaying=="true"&&track.url!=lastSongUrl){
+                if (!childProc||childProc.killed) {
+                    childProc = fork('update.js', [], {
+                        detached: true
+                    });
+                    childProc.on("message", function(m){
+                        if (m=="started") childProc.send(getPresence(track));
+                        else console.log(m);
+                    });
+                    childProc.on("exit", function(){
+                        console.log("Player stopped");
+                    });
+                }
+                else childProc.send(getPresence(track));
+                console.log("Track updated: "+ track.artist["#text"]+" - "+track.name);
+                lastSongUrl = track.url;
+            }
+            else if (childProc&&!childProc.killed&&lastSongUrl!="0"&&!track["@attr"]) {
+                childProc.kill();
+                lastSongUrl = "0";
+            }
+        });
+    }).on('error', (e) => {
+        console.error(`Got error: ${e.message}`);
     });
 }
 setInterval(update, settings.delay*1000);
